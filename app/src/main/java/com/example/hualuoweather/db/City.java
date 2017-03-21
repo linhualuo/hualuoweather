2b2c3595fd5e26a6acfee70a3fae550024adfe8a
Joseph <linhualuo@gmail.com>
2017-03-22 01:49:08 +0800

    创建了数据库和表的各项配置

---
+++ app/src/main/java/com/example/hualuoweather/db/City.java
@@ -0,0 +1,44 @@
+package com.example.hualuoweather.db;
+
+import org.litepal.crud.DataSupport;
+
+
+public class City extends DataSupport {
+
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
---
+++ app/src/main/java/com/example/hualuoweather/db/Province.java
@@ -0,0 +1,36 @@
+package com.example.hualuoweather.db;
+
+import org.litepal.crud.DataSupport;
+
+
+
+public class Province extends DataSupport {
+
+    private int id;
+    private String province;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}

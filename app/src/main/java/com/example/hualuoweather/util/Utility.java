34c4d0e2a881c6af929acd5fac25a65759152cd5
Joseph <linhualuo@gmail.com>
2017-03-22 17:16:23 +0800

    加入显示天气信息的功能 + 沉浸式状态栏

--- app/src/main/java/com/example/hualuoweather/util/Utility.java
+++ app/src/main/java/com/example/hualuoweather/util/Utility.java
@@ -6,6 +6,8 @@ import android.text.TextUtils;
 import com.example.hualuoweather.db.City;
 import com.example.hualuoweather.db.County;
 import com.example.hualuoweather.db.Province;
+import com.example.hualuoweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -79,5 +81,20 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
